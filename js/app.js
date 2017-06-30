0edde7128babbb5df83a8a3bb7d0bdf3fd7d923e
olpee <bienaimeluis97400@gmail.com>
2017-06-30 14:27:19 +0400

    creation

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let mots  = ["agentcarter","captainamerica","goloum","choubaka","jason","jeanbob"];
+let motsAleatoire = mots[Math.floor(Math.random()*mots.lenght)];
+
+let pendu = 6;
+let longmot = motstrouver.lenght;
+let ltrouver = "";
+
+for (let i = 0; i < motstrouver.lenght; i++)
+    ltrouver = "_"
+ 
+
